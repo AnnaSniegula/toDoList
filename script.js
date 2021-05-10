9a6d3ccd8704d4efa059ac05f234af29099e7f27
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-10 22:05:48 +0200

    js htmlString added

--- script.js
+++ script.js
@@ -6,7 +6,11 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li class="list__item>
+            <button class="list__button list__button--done js-done>
+            ${task.done ? "√" : ""}</button>
+            <span class="list__task${task.done ? "list__task--done" : ""}">${task.content}</span>
+            <button class="list__button list__button--remove js-remove> </button>
             </li>
             `;
         }
