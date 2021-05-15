af5031686899139e3f06144c1ca66c1d0662110e
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-15 21:58:10 +0200

    js format done

--- js/script.js
+++ js/script.js
@@ -55,16 +55,13 @@
         for (const task of tasks) {
             htmlString += `
             <li class="list__item">
-
             <button class="list__button list__button--done js-done">
             ${task.done ? "✓" : ""}</button>
-
             <span class="list__task${task.done ? " list__task--done" : ""}">
             ${task.content}</span>
-
             <button class="list__button list__button--remove js-remove">🗑</button>
             </li>
-            `
+              `
         };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
